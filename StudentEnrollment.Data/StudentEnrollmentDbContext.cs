10d55601cab5c9a9487d4392579b7d01544fa275
Julian Dave Shikui <juliandave1989@gmail.com>
2024-03-11 20:56:32 +0200

    git commit -m "first commit"

---
+++ StudentEnrollment.Data/StudentEnrollmentDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentEnrollment.Data
+{
+    public class StudentEnrollmentDbContext : IdentityDbContext
+    {
+        public StudentEnrollmentDbContext(DbContextOptions<StudentEnrollmentDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
