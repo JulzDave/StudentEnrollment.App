fd5903a381f370d55f581fdab118ad8f689662be
Julian Dave Shikui <juliandave1989@gmail.com>
2024-03-12 00:59:00 +0200

    initialized migrations

--- StudentEnrollment.Data/StudentEnrollmentDbContext.cs
+++ StudentEnrollment.Data/StudentEnrollmentDbContext.cs
@@ -1,9 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace StudentEnrollment.Data
 {
@@ -16,5 +15,25 @@ namespace StudentEnrollment.Data
         public DbSet<Course> Courses { get; set; }
         public DbSet<Student> Students { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
+    }
+
+    public class StudentEnrollmentDbContextFactory : IDesignTimeDbContextFactory<StudentEnrollmentDbContext>
+    {
+        public StudentEnrollmentDbContext CreateDbContext(string[] args)
+        {
+            // Get environment variable for connection string
+            string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<StudentEnrollmentDbContext>();
+            var connectionString = config.GetConnectionString("StudentEnrollmentDbConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new StudentEnrollmentDbContext(optionsBuilder.Options);
+        }
     }
 }
