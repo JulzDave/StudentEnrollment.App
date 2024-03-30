c9a10e37ce78b690ebd8aecb0fa127452fe214c2
Julian Dave Shikui <juliandave1989@gmail.com>
2024-03-31 00:43:35 +0300

    extended user class

---
+++ StudentEnrollment.Data/Migrations/20240330214105_ExtendedUserTable.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StudentEnrollment.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendedUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "45985e8b-2703-4909-ba1a-9e4641d9c02b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "bb7471f0-414e-4856-b40b-11a5f78247b0");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1b46baf7-7c2a-4a7f-b357-32ef3c58b2d9", null, "User", "USER" },
+                    { "9d729038-b2be-420f-8299-d2e84478d3a3", null, "Administrator", "ADMINISTRATOR" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1b46baf7-7c2a-4a7f-b357-32ef3c58b2d9");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9d729038-b2be-420f-8299-d2e84478d3a3");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "45985e8b-2703-4909-ba1a-9e4641d9c02b", null, "Administrator", "ADMINISTRATOR" },
+                    { "bb7471f0-414e-4856-b40b-11a5f78247b0", null, "User", "USER" }
+                });
+        }
+    }
+}
